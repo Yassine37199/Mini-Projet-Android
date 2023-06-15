c293c3575db1474b4f18c05dd0407f3ed623de25
Mega-Pc <yassineouri@samrise.cloud>
2023-06-15 18:13:50 +0100

    init project

---
+++ app/src/main/java/com/example/mini_projet/DetailActivity.java
@@ -0,0 +1,58 @@
+package com.example.mini_projet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.github.clans.fab.FloatingActionButton;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class DetailActivity extends AppCompatActivity {
+
+    TextView detailDesc, detailTitle;
+    ImageView detailImage;
+    FloatingActionButton deleteButton;
+    String key = "";
+    String imageUrl = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        detailDesc = findViewById(R.id.detailDescription);
+        detailImage = findViewById(R.id.detailImage);
+        detailTitle = findViewById(R.id.detailTitle);
+        deleteButton = findViewById(R.id.deleteButton);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null){
+            detailDesc.setText(bundle.getString("Description"));
+            detailTitle.setText(bundle.getString("Title"));
+            key = bundle.getString("key");
+            imageUrl = bundle.getString("image");
+            Glide.with(this).load(bundle.getString("Image")).into(detailImage);
+        }
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Android Data");
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                reference.child(key).removeValue();
+                Toast.makeText(DetailActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
